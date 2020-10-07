4581bfffe62357775b814f6aa3c96718e1dbcc21
Renata <renatacristovams@gmail.com>
2020-10-07 00:45:43 -0300

    xml changes

--- app/src/main/java/com/example/basicandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/basicandroidproject/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupToolbar(toolbarMain, R.string.app_name)
+
+
     }
 }
